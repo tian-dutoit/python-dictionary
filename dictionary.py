63c3c698bf2ac6e978b3952feec39ba6f07e2236
tian-dutoit <tiandutoit.nz@gmail.com>
2018-05-23 18:32:44 +1200

    initial check for closes word

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,6 @@
 import json
+from difflib import get_close_matches
+
 
 data = json.load(open("data.json")) 
 #  'r' read mode is the default
@@ -6,8 +8,21 @@ data = json.load(open("data.json"))
 def meaning(input_word):
     lower_word = input_word.lower()
     if lower_word in data:
-       print(data[lower_word])
+        return data[lower_word]
+    elif len(get_close_matches(input_word, data.keys())) > 0 :
+        closeresponse = input("Did you mean %s? Y/N: " % get_close_matches(input_word, data.keys())[0]).lower()
+        if closeresponse == "y":
+            newword = get_close_matches(input_word, data.keys()[0])
+            return newword
+        elif closeresponse == "n":
+            print("Word not found")
+        else:
+            print("Invalid response")
+
+    else:
+        print("The word is not in the dictionary")
+
 
 word = input("Please enter a word: ")
 
-meaning(word)
+print(meaning(word))
